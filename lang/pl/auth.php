804cba3c0d7eeffe369c1dd1fe38f7d0ee74de7f
LukaszJ <jozwiak.lukasz6@gmail.com>
2024-01-29 09:59:59 +0100

    added lang

--- lang/pl/auth.php
+++ lang/pl/auth.php
@@ -16,5 +16,16 @@ return [
     'failed' => 'Te dane uwierzytelniające nie są zgodne z naszymi danymi.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'login' => 'Zaloguj się',
+    'register' => 'Zarejestruj się',
+    'logout' => 'Wyloguj się',
+    'email' => 'Adres e-mail',
+    'Email' => 'Adres e-mail',
+    'Password' => 'Hasło',
+    'Remember me' => 'Zapamiętaj mnie',
+    'Forgot your password?' => 'Zapomniałeś hasła?',
+    'Log in' => 'Zaloguj się',
+    'Log Out' => 'Wyloguj się',
+    'Profile' => 'Profil',
 
 ];
